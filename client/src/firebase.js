03d33f1ab6ae8362597b54a4b0630c7e7a849d8e
manav-05-06 <manavkhandelwal72@gmail.com>
2025-11-01 21:00:57 +0530

    update UI or logic

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCLJ3Az5yx4je1ppwZJKrWikXOFg-LbpIM",
-  authDomain: "morse-code-6549a.firebaseapp.com",
-  projectId: "morse-code-6549a",
-  storageBucket: "morse-code-6549a.firebasestorage.app",
-  messagingSenderId: "561405376505",
-  appId: "1:561405376505:web:36b14aea004f866592af28",
-  measurementId: "G-SD7YT8ET0G"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
