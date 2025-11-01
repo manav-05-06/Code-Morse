32720a2572d437ce55f368a95ab267ed7d499a31
manav-05-06 <manavkhandelwal72@gmail.com>
2025-11-01 21:28:24 +0530

    update UI or logic

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,19 @@
-// firebaseConfig.js
+// firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCLJ3Az5yx4je1ppwZJKrWikXOFg-LbpIM",
-  authDomain: "morse-code-6549a.firebaseapp.com",
-  projectId: "morse-code-6549a",
-  storageBucket: "morse-code-6549a.firebasestorage.app",
-  messagingSenderId: "561405376505",
-  appId: "1:561405376505:web:36b14aea004f866592af28",
-  measurementId: "G-SD7YT8ET0G"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const analytics = getAnalytics(app);
+export default app;
